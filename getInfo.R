82b5ac028dcf59d4003e18cf6f6d6b149d3e3a68
lgarcia-camargo <56742501+lgarcia-camargo@users.noreply.github.com>
2020-11-23 17:47:45 -0800

    Updates to output

--- getInfo.R
+++ getInfo.R
@@ -12,12 +12,12 @@ analyze_data <- function(data_file, name, iters){
     	print("m_pl fit")
     	print(m_pl)
 
-    	bs_p = bootstrap_p(m_pl, threads = 6, no_of_sims = iters)
+    	bs_p = bootstrap_p(m_pl, threads = 4, no_of_sims = iters)
     	print("***************************")
     	print("bootstrap results")
     	print(bs_p)
 	print(bs_p$p)
-	write(c('bootstrap p-value:',bs_p$p), fileConn)
+	write(c('bootstrap p-value:',bs_p$p,''), fileConn)
 
 }
 
@@ -49,18 +49,18 @@ compare_dist <- function(data_file, name, iters){
 	print("Lnorm")
 	print(comp_lnorm$p_two_sided)
 	print(comp_lnorm$test_statistic)
-	write(c("Lnorm:",cat('p:',comp_lnorm$p_two_sided),cat('q:',comp_lnorm$test_statistic)),fileConn,append=TRUE, sep="\n\n")
+	write(c("Lnorm:",'p:',comp_lnorm$p_two_sided,'q:',comp_lnorm$test_statistic,''),fileConn,append=TRUE)
 
 	print("Pois")
 	print(comp_pois$p_two_sided)
 	print(comp_pois$test_statistic)
-	write(c("Lnorm:",cat('p:',comp_pois$p_two_sided),cat('q:',comp_pois$test_statistic)),fileConn,append=TRUE, sep="\n\n")
+	write(c("Lnorm:",'p:',comp_pois$p_two_sided,'q:',comp_pois$test_statistic,''),fileConn,append=TRUE)
 
 
 	print("Exp")
 	print(comp_exp$p_two_sided)
 	print(comp_exp$test_statistic)
-	write(c("Lnorm:",cat('p:',comp_exp$p_two_sided),cat('q:',comp_exp$test_statistic)),fileConn,append=TRUE, sep="\n\n")
+	write(c("Lnorm:",'p:',comp_exp$p_two_sided,'q:',comp_exp$test_statistic,''),fileConn,append=TRUE)
 }
 
 get_info <- function(data_file, name, iters){
@@ -72,15 +72,15 @@ get_info <- function(data_file, name, iters){
     	est = estimate_xmin(m_pl)
     	m_pl$setXmin(est)
 
-	write(c("Xmax:",data[1]),fileConn)
+	write(c("Xmax:",data[1],''),fileConn)
     	print("Xmax")
 	print(data[1])
 
-	write(c("Xmin:",est$xmin),fileConn,append=TRUE, sep="\n\n")
+	write(c("Xmin:",est$xmin,''),fileConn,append=TRUE, sep="\n\n")
 	print("Xmin")
 	print(est)
 	
-	write(c("Word count:",sum(data)),fileConn,append=TRUE, sep="\n\n")
+	write(c("Word count:",sum(data),''),fileConn,append=TRUE, sep="\n\n")
 	print("Word count")
 	print(sum(data))
 }
@@ -89,9 +89,7 @@ get_info <- function(data_file, name, iters){
 current_data <- readline(prompt="Data_file_name: ")
 results_file <- readline(prompt="Results_file_name: ")
 get_info(current_data,results_file)
-print('next')
 analyze_data(current_data, results_file, 5000)
-print('next')
 compare_dist(current_data, results_file)
 
 
